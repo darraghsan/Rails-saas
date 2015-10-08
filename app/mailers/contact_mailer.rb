10093404574669853f84df8c67c30f183b93806b
Darragh Carroll <darraghcarroll01@gmail.com>
2015-10-08 15:29:40 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,14 @@
+class ContactMailer < ActionMailer::base
+    
+    default to: 'darraghcarroll01@gmail.com'
+    
+    def contact_email(name, email, body)
+        
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact form message')
+    end
+
+end
