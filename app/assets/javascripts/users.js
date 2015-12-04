38705ed277f1ba5de6ad84c0d2eb760233fece0a
Darragh Carroll <darraghcarroll01@gmail.com>
2015-12-04 00:13:04 +0000

    Changed new action in profile controller

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -31,11 +31,11 @@ $(document).ready(function() {
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
-    // Get the token from the response:
-    var token = response.id;
-    // Add the token to the form:
-    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    // Submit the form:
-    f.get(0).submit(); 
-  }
+       // Get the token from the response:
+       var token = response.id;
+       // Add the token to the form:
+       f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+       // Submit the form:
+       f.get(0).submit(); 
+    }
 });
