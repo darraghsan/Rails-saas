d236a085f53ad12b8d872d9925cb4cf95a03a99c
Darragh Carroll <darraghcarroll01@gmail.com>
2015-12-17 00:55:54 +0000

    Improved users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user! #ensure user is logged in
     
     def index
+        @users = User.all #grab all users from db and save in users instance variable
     end
     
     def show
-        @user = User.find(params[:id])
+        @user = User.find(params[:id]) #locate user based on their id and save in instance variable.
     end
 end
